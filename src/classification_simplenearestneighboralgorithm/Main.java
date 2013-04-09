0934441ca7f8fcf588b99a140227f4b0144554c3
Michał <mmichalf@gmail.com>
2013-04-09 18:01:09 +0200

    init version
    empty

---
+++ src/classification_simplenearestneighboralgorithm/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classification_simplenearestneighboralgorithm;
+
+/**
+ *
+ * @author Michal
+ */
+public class Main
+{
+    
+}
