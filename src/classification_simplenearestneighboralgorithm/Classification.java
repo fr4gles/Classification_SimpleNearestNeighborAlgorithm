3cbba30e30cb2949111b9de7c06a6c876bc8f898
Michał <mmichalf@gmail.com>
2013-04-12 18:40:03 +0200

    some changes, wczytywanie z pliku

--- src/classification_simplenearestneighboralgorithm/Classification.java
+++ src/classification_simplenearestneighboralgorithm/Classification.java
@@ -4,11 +4,77 @@
  */
 package classification_simplenearestneighboralgorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  *
  * @author Michal
  */
 public class Classification
 {
+    public List<Class> classList;
     
+    public Classification()
+    {
+        classList = new ArrayList<>();
+    }
+    
+    public void addAndLearn(int id, List<Integer> list)
+    {
+        Integer index = searchIn(id);
+        
+        if(index != -1)
+            classList.get(index).add(list);
+        else
+            classList.add(new Class(id, list));
+    }
+    
+    public Integer searchIn(int id)
+    {
+        for(int i=0; i<classList.size(); ++i)
+            if(classList.get(i).Id == id)
+                return i;
+        
+        return -1;
+    }
+    
+    public void resolveProblem()
+    {
+        for(int k=0; k<classList.get(classList.size()-1).points.size(); ++k)
+        {
+            for(int i=0; i<classList.size()-2; ++i)
+            {
+                for(int j=0; j<classList.get(i).points.size(); ++j)
+                {
+                    
+                }
+            }
+        }
+    }
+    
+    public Double liczOdleglosc()
+    {
+        
+    }
+}
+
+class Class
+{
+    public List<List<Integer>> points;
+    public Integer Id;
+    
+    public Class(Integer id, List<Integer> p)
+    {
+        points = new ArrayList<>();
+        
+        this.Id = id;
+        add(p);
+    }
+    
+    public void add(List<Integer> p)
+    {
+        points.add(p);
+    }
 }
--- src/classification_simplenearestneighboralgorithm/Main.java
+++ src/classification_simplenearestneighboralgorithm/Main.java
@@ -4,11 +4,96 @@
  */
 package classification_simplenearestneighboralgorithm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  *
  * @author Michal
  */
 public class Main
 {
+    private static int LicabaElementow = 0;
+    private static Boolean TEST = Boolean.FALSE;
     
+    public static void main(String[] args)
+    {        
+        if (args.length < 2) // jesli zla ilosc argumentow wejsciowych to poinforuj o tym uzytkownika
+        {
+            System.err.println("BLAD, zla ilosc argumentow wejsciowych. Podaj <nazwa_pliku> <n_size>");
+            System.exit(1);
+        }
+
+        try
+        {
+            Main.setLicabaElementow(Integer.parseInt(args[1])); // czytanie z wejscia wielkosc tafli
+        }
+        catch (NumberFormatException e)
+        {
+            System.err.println("BLAD ZLY ROZMIAR" + e.getMessage());
+        }
+        
+        
+        List<Integer> pointList = new ArrayList<>();
+        
+        int tmpLicznik = 0;
+        
+        Classification c = new Classification();
+        
+        File file = new File(args[0]); // użycie pliku wejsciowego do odczytu danych
+        try
+        {
+            Scanner sc = new Scanner(file).useDelimiter("\\D+"); // pobieranie tylko liczb ...
+            while (sc.hasNext())
+            {
+                Integer tmpId = sc.nextInt();
+                List<Integer> tmpPointList = new ArrayList<>();
+                for(int i=1; i<Main.LicabaElementow; ++i)
+                {
+                    tmpPointList.add(sc.nextInt());
+                }
+                c.addAndLearn(tmpId, tmpPointList);
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.err.println("BLAD Z PARSOWANIEM PLIKU WEJSCIOWEGO czy PLIK istnieje??? -> " + ex.getMessage());
+        }
+        catch (NoSuchElementException ex)
+        {
+            if (Main.getTEST())
+            {
+                System.err.println("BLAD ZLY FORMAT KRAWEDZI -> " + ex.getMessage());
+            }
+        }
+        
+    }
+
+    /**
+     * @return the LicabaElementow
+     */
+    public static int getLicabaElementow()
+    {
+        return LicabaElementow;
+    }
+
+    /**
+     * @param aLicabaElementow the LicabaElementow to set
+     */
+    public static void setLicabaElementow(int aLicabaElementow)
+    {
+        LicabaElementow = aLicabaElementow;
+    }
+
+    /**
+     * @return the TEST
+     */
+    public static Boolean getTEST()
+    {
+        return TEST;
+    }
 }
